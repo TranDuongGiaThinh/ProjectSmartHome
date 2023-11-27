4c001b73c5602ef3dc4bc569542c51cbdd58349a
Thanh-Huy <huycherry14@gmail.com>
2023-11-27 10:28:01 +0700

    remove all file

--- lib/views/color_utils.dart
+++
@@ -1,9 +0,0 @@
- import 'package:flutter/material.dart';
-
-hexStringToCoLor(String hexColor){
-  hexColor=hexColor.toUpperCase().replaceAll("#", "");
-  if(hexColor.length==6){
-    hexColor="FF$hexColor";
-    return Color(int.parse(hexColor,radix: 16));
-  }
- }
